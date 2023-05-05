d8c15ee3a6e17ea4c0dd4df213a44aa6260219a4
npittigl <npittigl@gmail.com>
2023-05-04 21:08:36 -0400

    mobile nav menu

---
+++ script.js
@@ -0,0 +1,23 @@
+// JavaScript for menu to slide out/in on smaller screens:
+
+// 1. target hamburger menu button 
+const menuButton = document.querySelector('.toggle');
+const menuItem = document.querySelectorAll('.menuItem');
+// get ul element from HTML
+const menuList = document.getElementById('menu');
+const menuUl = document.getElementById('menuUl');
+
+// 3. add event listener to the menu button
+menuButton.addEventListener('click', function () {
+    // target button's child <i>; toggles class to replace hamburger with 'x' icon & vice versa
+    this.children[0].classList.toggle('fa-times');
+
+    // adds/removes class to allow ul to slide in/out
+    menuList.classList.toggle('slideOut');
+});
+
+// 4. Add event listener to nav links
+menuUl.addEventListener('click', function() {
+    menuList.classList.toggle('slideOut');
+    menuButton.children[0].classList.toggle('fa-times');
+});
