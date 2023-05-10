1610602324ed55819461c8bf600a59db81ca451f
npittigl <npittigl@gmail.com>
2023-05-10 15:52:27 -0400

    changes to ham menu, javascript

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-// JavaScript for menu to slide out/in on smaller screens:
+// JS FOR SLIDE OUT/IN MENU ON SMALLER SCREENS:
 
-// 1. target elements
-const menuButton = document.querySelector('.toggle');
-const menuList = document.getElementById('menu');
+// 1. Target menu elements:
+const navMenu = document.getElementById('navMenu');
+const menuButton = document.getElementById('toggle');
 const menuUl = document.getElementById('menuUl');
 
-// 2. add event listener to the menu button
-menuButton.addEventListener('click', function () {
-    // target button's child <i>; toggles class to replace hamburger with 'x' icon & vice versa
-    this.children[0].classList.toggle('fa-times');
+// 2. Add event listener to nav menu:
+navMenu.addEventListener('click', function (event) {
+    // if the event target is an <i>, button or <a> element, then event is triggered
+    if (event.target.tagName === 'I' || event.target.tagName === 'BUTTON' || event.target.tagName === 'A') {
+        // toggles class to replace hamburger with 'x' icon & vice versa
+        menuButton.children[0].classList.toggle('fa-times');
 
-    // adds/removes class to allow ul to slide in/out
-    menuList.classList.toggle('slideOut');
-});
+        // update aria-label
+        if (menuButton.children[0].className === 'fa fa-bars fa-times') {
+            menuButton.setAttribute('aria-label', 'hide navigation menu')
+            menuButton.setAttribute('aria-expanded', true);
+        } else {
+            menuButton.setAttribute('aria-label', 'show navigation menu');
+            menuButton.setAttribute('aria-expanded', false);
+        }
 
-// 3. Add event listener to nav links
-menuUl.addEventListener('click', function() {
-    menuList.classList.toggle('slideOut');
-    menuButton.children[0].classList.toggle('fa-times');
-});
+        // removes/adds visibility of ul when on/off screen
+        menuUl.classList.toggle('vh');
+        // adds/removes class to allow menu to slide in/out
+        navMenu.classList.toggle('slideOut');
+    }
+})
 
-// JS to clear submission form after use
+// JS TO CLEAR FORM AFTER SUBMISSION:
 window.onbeforeunload = () => {
     for (const form of document.getElementsByTagName('form')) {
         form.reset();
     }
 }
+
